56a1492d3acf4f72ee1fae5892b806a3a813b816
malamsyah <hi@malamsyah.com>
2022-05-05 09:31:49 +0700

    Remove move to book options

--- src/components/BookshelfChanger.js
+++ src/components/BookshelfChanger.js
@@ -10,9 +10,6 @@ const BookshelfChanger = ({ book, onUpdateShelf }) => {
   return (
     <div className="book-shelf-changer">
       <select value={value} onChange={handleOnChange}>
-        <option value="none" disabled>
-          Move to...
-        </option>
         <option value="currentlyReading">Currently Reading</option>
         <option value="wantToRead">Want to Read</option>
         <option value="read">Read</option>
